d8b7b4a55dfe2ae3e91f85c2c63dea78a46fad39
Aravind0211 <113758941+Aravind0211@users.noreply.github.com>
2024-06-25 10:28:27 +0530

    Update OrderDaprConfiguration.java

--- src/main/java/dev/dberenguer/dapr/order/configuration/OrderDaprConfiguration.java
+++ src/main/java/dev/dberenguer/dapr/order/configuration/OrderDaprConfiguration.java
@@ -1,12 +1,11 @@
 package dev.dberenguer.dapr.order.configuration;
 
 import io.dapr.client.DaprClient;
-import io.dapr.client.DaprClientBuilder;
+import io.dapr.exceptions.DaprException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.BeanCreationException;
 
 import java.util.Map;
 
@@ -14,41 +13,25 @@ import java.util.Map;
 @Configuration
 public class OrderDaprConfiguration {
 
-    @Value("${dberenguer.dapr.pub-sub.name}")
-    private String daprPubSubName;
-
-    @Value("${dberenguer.dapr.pub-sub.topic.name}")
-    private String daprPubSubTopicName;
-
     @Value("${dberenguer.dapr.secret-store.name}")
     private String secretStoreName;
 
     @Bean
-    public String daprPubSubNameBean() {
-        return this.daprPubSubName;
-    }
-
-    @Bean
-    public String daprPubSubTopicNameBean() {
-        return this.daprPubSubTopicName;
-    }
-
-    @Bean
-    public DaprClient orderDaprClient() {
+    public DaprClient daprClient() {
         return new DaprClientBuilder().build();
     }
 
     @Bean
-    public String secretDaprClient(final DaprClient daprClient) {
+    public String secretDaprClient(DaprClient daprClient) {
         try {
             // Attempt to fetch the secret
-            final Map<String, String> secret = daprClient.getSecret(this.secretStoreName, "mySecret").block();
+            Map<String, String> secret = daprClient.getSecret(this.secretStoreName, "mySecret").block();
             log.info("Fetched Secret: {}", secret);
             return secret.toString();
-        } catch (Exception e) {
-            // Log the exception and throw a BeanCreationException
+        } catch (DaprException e) {
+            // Log the exception and throw a RuntimeException
             log.error("Failed to fetch secret from Dapr", e);
-            throw new BeanCreationException("Failed to instantiate secretDaprClient", e);
+            throw new RuntimeException("Failed to fetch secret from Dapr", e);
         }
     }
 }
